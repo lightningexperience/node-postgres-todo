d283db1c206f54bfd58e304e3898be0101933c50
lightningexperience <lightningexperience@users.noreply.github.com>
2021-03-16 17:53:59 -0400

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const connectionString = process.env.DATABASE_URL || 'mysql://wm4jfhlhd06yrpzk:ggevaba3yzcx2pif@g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/iabmgvpuekopvhlv';
 
 const client = new pg.Client(connectionString);
 client.connect();
